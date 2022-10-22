ca7c69c27a58206a2a4f8e2cdc1b38e751d172d7
Evy Garden <evysgarden@protonmail.com>
2022-10-22 18:45:28 +0200

    reworked url checking to use reqwest

--- src/utils.rs
+++ src/utils.rs
@@ -1,68 +1,53 @@
 use core::fmt;
-use hyper::{Client, Uri};
-use std::{
-    error::Error,
-    fs,
-    future::{self, Future},
-    path::Path,
-    str::FromStr,
-    time::Duration,
-};
-use tokio::runtime::Builder;
+use std::{error::Error, fs, path::Path, time::Duration};
 use yaml_rust::{Yaml, YamlLoader};
+use reqwest::StatusCode;
 
-type DynResult<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
+type BoxError = Box<dyn std::error::Error + Send + Sync>;
 
 #[derive(Debug)]
-pub struct BadHttpStatusError(pub hyper::StatusCode);
+pub struct HttpStatusError{
+    pub url: String,
+    pub status: StatusCode
+}
 
-impl fmt::Display for BadHttpStatusError {
+impl fmt::Display for HttpStatusError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Got status code {}", self.0)
+        write!(f, "Got status code {} for {}", self.status, self.url)
     }
 }
 
-impl Error for BadHttpStatusError {}
+impl Error for HttpStatusError {}
 
-pub fn yaml_from_file(path: &Path) -> Result<Yaml, Box<dyn Error>> {
+pub fn yaml_from_file(path: &Path) -> Result<Yaml, BoxError> {
     let config_str = fs::read_to_string(path)?;
     let config_yml = YamlLoader::load_from_str(config_str.as_str())?;
 
     Ok(config_yml[0].clone())
 }
 
-pub async fn check_url(url: &'static str) -> DynResult<()> {
-    let client = Client::new();
-    let result = client.get(Uri::from_str(url)?).await?;
-    println!("Got Status code: {}", result.status());
-    if result.status().is_success() {
-        Ok(())
-    } else {
-        Err(Box::new(BadHttpStatusError(result.status())))
+pub fn check_url(url: &str, timeout: Duration) -> Result<(), BoxError> {
+    let client = reqwest::blocking::Client::new();
+    match client.get(url).timeout(timeout).send() {
+        Ok(result) => {
+            if result.status().is_success() {
+                Ok(())
+            } else {
+                Err(Box::new(HttpStatusError{
+                    url: result.url().to_string(),
+                    status: result.status(),
+                }))
+            }
+        }
+        Err(err) => Err(Box::new(err)),
     }
 }
 
-pub fn check_urls(urls: Vec<&str>, timeout: Duration) -> Result<(), Box<dyn Error>> {
-    let runtime = Builder::new_multi_thread()
-        .worker_threads(4)
-        .thread_name("url-checker")
-        .enable_time()
-        .build()
-        .unwrap();
-
-    let _eg = runtime.enter();
-
-    for &url in &urls {
-        println!("Checking {}", url);
-        let timeout = tokio::time::timeout(
-            timeout,
-            check_url(Box::leak(url.to_owned().into_boxed_str())),
-        );
-
-        runtime.spawn(timeout);
+pub fn check_urls(urls: &Vec<&str>, timeout: Duration) -> Result<(), BoxError> {
+    for url in urls {
+        if let Err(err) = check_url(url, timeout) {
+            return Err(err);
+        }
     }
-
-    runtime.shutdown_timeout(timeout * urls.len().try_into().unwrap() * 2);
-
     Ok(())
 }
