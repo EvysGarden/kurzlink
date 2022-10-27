18e200b501b4f519f86b9e7e033a648e36d5fdce
Evy Garden <evysgarden@protonmail.com>
2022-10-27 14:19:53 +0200

    added some error handling in main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-use crate::config::Config;
-use std::{path::Path};
+use crate::{config::Config, error::ValidationError};
+use std::{path::Path, process::exit};
 
 mod config;
+mod error;
 mod utils;
 
 fn main() {
-    let config = Config::new(Path::new("kurzlink.yml"));
+    let config = Config::new(Path::new("kurzlink.yml")).unwrap_or_else(|v| {
+        println!("Invalid config file: {:?}", v);
+        exit(-1);
+    });
     // println!("{:?}", config);
-    config.check_links();
+    if let Err(validation_error) = config.validate() {
+        match &validation_error {
+            ValidationError::DuplicateSources(v) => println!("Found duplicate sources: {:?}", v),
+            ValidationError::DuplicateDestinations(v) => {
+                println!("Found duplicate destinations: {:?}", v)
+            }
+            ValidationError::NetworkError(v) => println!("Network error: {}", v),
+        }
+        exit(validation_error.error_code());
+    }
 }
