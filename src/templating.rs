5b242311cf603a6f1e1e3a7947fd15fb7fa0261d
Iamjava <jan.vaorin@tu-dortmund.de>
2022-10-31 11:56:12 +0100

    added writing capabilities

--- src/templating.rs
+++ src/templating.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
+use std::io::Write;
 use minijinja::{Environment, context};
 use crate::config::shortlink;
 use std::fs;
+use std::fs::File;
 use std::path::Path;
 
 pub fn print_kurzlink_page_from_template<P: AsRef<Path>>(link: &shortlink::Shortlink, template_path: P)->Result<String,Box<dyn Error>>{
@@ -12,19 +14,31 @@ pub fn print_kurzlink_page_from_template<P: AsRef<Path>>(link: &shortlink::Short
     Ok(tmpl.render(context!(link => link))?)
 }
 
+// kein plan ob das worked
+pub fn write_html<P: AsRef<str>>(text_to_write: P,filename: P )->Result<(),Box<dyn std::error::Error>>{
+    let path = format!("out/{}.html", filename.as_ref());
+    let mut output = File::create(path)?;
+    write!(output, "{}", text_to_write.as_ref());
+    Ok(())
+}
+
 
 #[cfg(test)]
 mod tmp_tests {
     use std::path::Path;
-    use crate::templating::print_kurzlink_page_from_template;
+    use crate::templating::{print_kurzlink_page_from_template, write_html};
     use crate::Config;
 
     #[test]
-    fn it_works() {
+    fn test_render() {
         let links = Config::new("kurzlink.yml").expect("Invalid shortlink yaml file");
         let link_to_print = links.shortlinks.get(2).unwrap();
         let rendered_template= print_kurzlink_page_from_template(&link_to_print,"gitlab_redirect_page.template").unwrap();
         print!("{rendered_template}");
         assert!(rendered_template.contains(&link_to_print.destination));
+    }
+    #[test]
+    fn test_dinge() {
+        write_html("test","TESTNAME").unwrap();
     }
 }
