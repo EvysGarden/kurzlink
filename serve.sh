a8e920f757e33c4a245ced22da51adedc19f6353
Evy Garden <evysgarden@protonmail.com>
2022-11-01 13:14:09 +0100

    updated script

--- serve.sh
+++ serve.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-rm -rf public
+KL_PWD="$(pwd)"
+KL_BASEPATH="${KL_PWD}/servetest"
+
 cargo build
-./target/debug/kurzlink --generate -o public
-cd public
+./target/debug/kurzlink --generate -o "${KL_BASEPATH}"
+cd "${KL_BASEPATH}"
 python -m http.server
-cd ..
+cd "${KL_PWD}"
+rm -rf "${KL_BASEPATH}"
