5c159a60497921515af31d1b84f5f994d159b2fd
evysgarden <evysgarden@protonmail.com>
2022-11-04 11:14:57 +0100

    use serde serialization

--- src/config/shortlink.rs
+++ src/config/shortlink.rs
@@ -5,26 +5,5 @@ pub struct Shortlink {
     pub sources: Vec<String>,
     pub destination: String,
     pub tags: Vec<String>,
-    pub check_connection: bool,
-}
-
-impl From<&serde_yaml::Value> for Shortlink {
-    fn from(yaml: &serde_yaml::Value) -> Self {
-        Shortlink {
-            sources: yaml["sources"]
-                .as_sequence()
-                .unwrap()
-                .iter()
-                .map(|v| v.as_str().unwrap().to_string())
-                .collect(),
-            destination: yaml["destination"].as_str().unwrap().to_string(),
-            tags: yaml["tags"]
-                .as_sequence()
-                .unwrap()
-                .iter()
-                .map(|v| v.as_str().unwrap().to_string())
-                .collect(),
-            check_connection: yaml["check"].as_bool().unwrap_or(true),
-        }
-    }
+    pub check: bool,
 }
--- src/config/tag.rs
+++ src/config/tag.rs
@@ -1,14 +1,7 @@
-#[derive(Debug)]
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Tag {
     pub description: String,
     pub image: String,
-}
-
-impl From<&serde_yaml::Value> for Tag {
-    fn from(yaml: &serde_yaml::Value) -> Self {
-        Tag {
-            description: yaml["description"].as_str().unwrap().to_string(),
-            image: yaml["image"].as_str().unwrap().to_string(),
-        }
-    }
 }
