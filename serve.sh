43714d76b29a08a44d0af9774c1c8488d564a048
evysgarden <evysgarden@protonmail.com>
2022-11-04 11:15:30 +0100

    let serve script generate vanitymap

--- serve.sh
+++ serve.sh
@@ -2,9 +2,10 @@
 
 KL_PWD="$(pwd)"
 KL_BASEPATH="${KL_PWD}/servetest"
+KL_VANITYPATH="${KL_BASEPATH}/vanitymap.json"
 
 cargo build
-./target/debug/kurzlink -g -o "${KL_BASEPATH}"
+./target/debug/kurzlink -g -o "${KL_BASEPATH}" -m "${KL_VANITYPATH}"
 cd "${KL_BASEPATH}"
 python3 -m http.server
 cd "${KL_PWD}"
