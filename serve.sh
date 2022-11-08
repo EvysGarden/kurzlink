56eb18dacc0f8b5f7df9a6c74ac1f5de1020344b
Evy Garden <evysgarden@protonmail.com>
2022-11-08 02:43:14 +0100

    only serve when build and run as success

--- serve.sh
+++ serve.sh
@@ -4,8 +4,7 @@ KL_PWD="$(pwd)"
 KL_BASEPATH="${KL_PWD}/servetest"
 KL_VANITYPATH="${KL_BASEPATH}/vanitymap.json"
 
-cargo build
-./target/debug/kurzlink -g -o "${KL_BASEPATH}" -m "${KL_VANITYPATH}"
+cargo run -- -g -o "${KL_BASEPATH}" -m "${KL_VANITYPATH}" || exit
 cd "${KL_BASEPATH}"
 python3 -m http.server
 cd "${KL_PWD}"
