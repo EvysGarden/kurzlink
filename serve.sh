a745fb094adbe282d097d38a63c2656af2632de4
evysgarden <evysgarden@protonmail.com>
2022-12-12 20:10:37 +0100

    added vanitymap.yaml from fachschaften

--- serve.sh
+++ serve.sh
@@ -3,8 +3,9 @@
 KL_PWD="$(pwd)"
 KL_BASEPATH="${KL_PWD}/servetest"
 KL_VANITYPATH="${KL_BASEPATH}/vanitymap.json"
+KL_CONFIGFILE="${KL_PWD}/vanitymap.yaml"
 
-cargo run -- -g -o "${KL_BASEPATH}" -m "${KL_VANITYPATH}" || exit
+cargo run -- -g -o "${KL_BASEPATH}" -m "${KL_VANITYPATH}" -c "${KL_CONFIGFILE}" -n || exit
 cd "${KL_BASEPATH}"
 python3 -m http.server
 cd "${KL_PWD}"
