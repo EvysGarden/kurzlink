c840133487af81ef76a4f4021f23059094d97b0b
Falk Rehse <falk.rehse@tu-dortmund.de>
2023-01-07 20:02:08 +0100

    Made tag image optional

--- src/config/tag.rs
+++ src/config/tag.rs
@@ -3,5 +3,5 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Tag {
     pub description: String,
-    pub image: String,
+    pub image: Option<String>,
 }
